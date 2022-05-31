57060ed538ac072afde2fff038fd9e4461e41ca7
kamyab rouhifar <krouhifar@myseneca.ca>
2022-05-31 12:25:09 -0400

    Version 0.0.1 Hello World

---
+++ HelloWorld/src/Main.java
@@ -0,0 +1,32 @@
+package HelloWorld;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /**
+         * <h1>Hello, World!</h1>
+         * <p>
+         * The HelloWorld program implements an application that
+         * simply displays "Hello World!" to the standard output print.
+         * </p>
+         *
+         *
+         * @author  Kamyab Rouhifar
+         * @version 0.0.1
+         * @since   2022-05-31]
+         * {@link HelloWorld}
+         */
+
+        // This is a comment syntax
+
+        System.out.println("Hello World");
+        // Same with previous print line
+        System.out.print("Hello World\n");
+
+        // This is an error syntax
+        // System.out.println(""Hello World"");
+
+        // Right syntax using '\'
+        System.out.println("\"Hello World\"");
+    }
+}
